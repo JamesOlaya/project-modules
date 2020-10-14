ea24daad88b669fb5ea4779b0da810d0b5c069cb
msolanogithub <msolano2505@gmail.com>
2020-10-14 11:04:00 -0500

    +First commit

---
+++ config/asgard/isite/config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+  'name' => 'Isite',
+
+  'modulesToManagePermissions' => [
+    "Core",
+    "Ilocations",
+    "Icustom",
+    "Iredirect",
+    "Isearch",
+    "Inotification",
+    "Isite",
+    "Iblog",
+    "Ibanners",
+    "Iprofile",
+    "Media",
+    "Slider",
+    "Core",
+    "Iforms",
+    "Page",
+    "Setting",
+    "Menu",
+    "Tag",
+    "Translation",
+    "Workshop",
+    "User",
+    "Dashboard",
+    "Workshop"
+  ],
+];
